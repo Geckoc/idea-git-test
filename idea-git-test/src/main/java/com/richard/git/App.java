0a49a36d28fb3c950fe8238a040e6096ddb977ab
Gecko <Geckochanilix@gmail.com>
2021-02-24 19:25:19 +0800

    commit project to remote Repository

--- idea-git-test/src/main/java/com/richard/git/App.java
+++ idea-git-test/src/main/java/com/richard/git/App.java
@@ -7,5 +7,6 @@ package com.richard.git;
 public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        System.out.println( "Hello Git!" );
     }
 }
